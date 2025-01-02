d69b000ee2d92fc3429f3ca4b92f6108b82d548a
Khai Do <zaro0508@gmail.com>
2025-01-02 11:26:04 -0800

    [IT-3995] Setup WAF for application
    Web application firewall for agora app, enabled with
    the AWS managed Baseline core rule set[1] to protect again
    OWASP top 10 vunerablities[2]. Additional rules[3] can
    be added by updating the WebAcl rules object.
    
    [1] https://docs.aws.amazon.com/waf/latest/developerguide/aws-managed-rule-groups-baseline.html#aws-managed-rule-groups-baseline-crs
    [2] https://owasp.org/www-project-top-ten/
    [3] https://docs.aws.amazon.com/waf/latest/developerguide/aws-managed-rule-groups-baseline.html

--- src/load_balancer_stack.py
+++ src/load_balancer_stack.py
@@ -3,6 +3,7 @@ import aws_cdk as cdk
 from aws_cdk import (
     aws_ec2 as ec2,
     aws_elasticloadbalancingv2 as elbv2,
+    aws_wafv2 as wafv2
 )
 
 from constructs import Construct
@@ -10,7 +11,7 @@ from constructs import Construct
 
 class LoadBalancerStack(cdk.Stack):
     """
-    API Gateway to allow access to ECS app from the internet
+    Load Balancer to allow access to ECS app from the internet
     """
 
     def __init__(
@@ -21,6 +22,50 @@ class LoadBalancerStack(cdk.Stack):
         self.alb = elbv2.ApplicationLoadBalancer(
             self, "AppLoadBalancer", vpc=vpc, internet_facing=True
         )
+
+        # WAF to protect against common web attacks (OWASP Top 10)
+        web_acl = wafv2.CfnWebACL(self,"WebAcl",
+            name="AppWebAcl",
+            default_action=wafv2.CfnWebACL.DefaultActionProperty(
+                allow=wafv2.CfnWebACL.AllowActionProperty()
+            ),
+            scope="REGIONAL",
+            visibility_config=wafv2.CfnWebACL.VisibilityConfigProperty(
+                cloud_watch_metrics_enabled=True,
+                metric_name="AWSManagedRulesCommonRuleSet",
+                sampled_requests_enabled=True
+            ),
+            rules=[wafv2.CfnWebACL.RuleProperty(
+                name="AWSManagedRulesCommonRuleSet",
+                priority=0,
+                statement=wafv2.CfnWebACL.StatementProperty(
+                    managed_rule_group_statement=wafv2.CfnWebACL.ManagedRuleGroupStatementProperty(
+                        name="AWSManagedRulesCommonRuleSet",
+                        vendor_name="AWS",
+                        excluded_rules=[]
+                    )
+                ),
+                action=wafv2.CfnWebACL.RuleActionProperty(
+                    block={}
+                ),
+                visibility_config=wafv2.CfnWebACL.VisibilityConfigProperty(
+                    cloud_watch_metrics_enabled=True,
+                    metric_name="AWSManagedRulesCommonRuleSet",
+                    sampled_requests_enabled=True
+                ),
+                override_action=wafv2.CfnWebACL.OverrideActionProperty(
+                    none={}
+                )
+            )]
+
+        )
+
+        wafv2.CfnWebACLAssociation(
+            self, "web_acl_association",
+            resource_arn=self.alb.load_balancer_arn,
+            web_acl_arn=web_acl.attr_arn
+        )
+
         cdk.CfnOutput(
             self,
             "LoadBalancerDns",
