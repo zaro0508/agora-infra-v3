1e819f760356b8468b50e8d52c64e4da4782a7c6
Hallie Swan <user.email=26949006+hallieswan@users.noreply.github.com>
2025-01-30 17:10:34 +0000

    IT-4243: improve naming per review

--- app.py
+++ app.py
@@ -21,21 +21,21 @@ match environment:
         environment_variables = {
             "VPC_CIDR": "10.254.174.0/24",
             "FQDN": "prod.agora.io",
-            "CERTIFICATE_RESOURCE_ID": "69b3ba97-b382-4648-8f94-a250b77b4994",
+            "CERTIFICATE_ID": "69b3ba97-b382-4648-8f94-a250b77b4994",
             "TAGS": {"CostCenter": "Agora / 112300"},
         }
     case "stage":
         environment_variables = {
             "VPC_CIDR": "10.254.173.0/24",
             "FQDN": "stage.agora.io",
-            "CERTIFICATE_RESOURCE_ID": "69b3ba97-b382-4648-8f94-a250b77b4994",
+            "CERTIFICATE_ID": "69b3ba97-b382-4648-8f94-a250b77b4994",
             "TAGS": {"CostCenter": "Agora / 112300"},
         }
     case "dev":
         environment_variables = {
             "VPC_CIDR": "10.254.172.0/24",
             "FQDN": "dev.agora.io",
-            "CERTIFICATE_RESOURCE_ID": "e8093404-7db1-4042-90d0-01eb5bde1ffc",
+            "CERTIFICATE_ID": "e8093404-7db1-4042-90d0-01eb5bde1ffc",
             "TAGS": {"CostCenter": "Agora / 112300"},
         }
     case _:
@@ -175,7 +175,7 @@ apex_stack = LoadBalancedServiceStack(
     cluster=ecs_stack.cluster,
     props=apex_props,
     load_balancer=load_balancer_stack.alb,
-    certificate_resource_id=environment_variables["CERTIFICATE_RESOURCE_ID"],
+    certificate_id=environment_variables["CERTIFICATE_ID"],
     health_check_path="/health",
 )
 apex_stack.add_dependency(app_stack)
